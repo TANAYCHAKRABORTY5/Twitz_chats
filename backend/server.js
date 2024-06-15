1d82075a82f2d71c371cbb159e5b26701c5f6037
Tanay Chakraborty <tanaychakraborty.55@gmail.com>
2024-06-15 13:14:26 +0530

    testing5

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ["https://twist-chatz-1.onrender.com", "http://localhost:3000"],
+    origin: "https://twist-chatz-1.onrender.com",
   },
 });
 
